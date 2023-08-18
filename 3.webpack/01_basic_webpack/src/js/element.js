6492b31a9687ae3b048d80d3944127bf2df9b3bc
hjklala <306549749@qq.com>
2023-08-19 00:12:11 +0800

    webpack的loader呵plugin

--- 3.webpack/01_basic_webpack/src/js/element.js
+++ 3.webpack/01_basic_webpack/src/js/element.js
@@ -1,9 +1,16 @@
 //内联方式使用css-loader
 // import "css-loader!../css/style.css";
 import "../css/style.css";
+import headimg from "../img/2.jpg";
 
 const EL=document.createElement('div')
 EL.className='title'
 EL.innerHTML='你好啊，lalalalala'
 
+//设置图片
+const imgEL=document.createElement('img')
+imgEL.src=headimg
+
+
 document.body.appendChild(EL)
+document.body.appendChild(imgEL)
--- 3.webpack/01_basic_webpack/webpack.config.js
+++ 3.webpack/01_basic_webpack/webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
+const {CleanWebpackPlugin}=require("clean-webpack-plugin")
+const HtmlWebpackPlugin=require("html-webpack-plugin")
 
 //webpack配置文件
 module.exports = {
+  //设置模式 development 开发阶段设置，production 准备打包上线的时候设置
+  //mode:"development",
+  //设置source-map 建立js映射文件，方便调试与发现错误
+  //devtool:"source-map",
   //入口
   entry: "./src/main.js",
   //打包出口
   output: {
     path: path.resolve(__dirname, "./build"),
-    filename: "bundle.js"
+    filename: "js/bundle.js"
   },
   //loader配置
   module:{
@@ -16,10 +22,59 @@ module.exports = {
         test:/\.css$/,  //正则表达式
         //loader写法语法糖
         //loader:"css-loader"
+
+        //use执行顺序从后往前
+        //postcss配置单独提出在postcss.config.js中 preset-env中包含autoprefixer
         use:[
-          "css-loader"
+          "style-loader",
+          "css-loader",
+          "postcss-loader"
         ]
-      }
+        //postcss配置不提出写法
+        // use:[
+        //   "style-loader",
+        //   "css-loader",
+        //   {
+        //     loader:"postcss-loader",
+        //     options:{
+        //       postcssOptions:{
+        //         plugins:[
+        //           require('autoprefixer')
+        //         ]
+        //       }
+        //     }
+        //   }
+        // ]
+      },
+
+
+      //webpack5 使用asset来替代file-loader和url-loader
+      {
+        test:/\.(jpe?g|png|gif|svg)$/,
+        //类型 asset =>自动选择 asset/resource ->file-loader asset/inline -> url-loader
+        type:"asset",
+        //生成路径及名字配置placeHolder [name]原文件名字 [hash:6]六位hash值 [ext]文件后缀
+        generator:{
+          filename:'img/[name]_[hash:6][ext]'
+        },
+        //限制配置
+        parser:{
+          dataUrlCondition:{
+            maxSize:50*1024
+          }
+        }
+      },     
     ]
-  }
+  },
+  //插件的注册
+  plugins:[
+    //打包自动清理之前打包的文件 插件
+    new CleanWebpackPlugin(),
+    //index.html打包插件(可以自动生成)
+    new HtmlWebpackPlugin({
+      // template:'模板所在位置'  按照你所提供的模板生成index.html
+      //修改生成出来的index.html的title
+      title:"哈哈哈哈哈"
+    })
+  ]
 }
