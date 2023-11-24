5a475341a1b87a60ec6a408f39eab48943caa331
hjklala <306549749@qq.com>
2023-11-24 16:22:56 +0800

    devsever和代理配置

--- 3.webpack/01_basic_webpack/webpack.config.js
+++ 3.webpack/01_basic_webpack/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const {CleanWebpackPlugin}=require("clean-webpack-plugin")
 const HtmlWebpackPlugin=require("html-webpack-plugin")
-// const {DefinPlusgin} =require("webpack")
+const {DefinePlugin} =require("webpack")
 // const CopyWebpackPlugin =require("copy-webpack-plugin")
 const {VueLoaderPlugin}=require('vue-loader/dist/index')
 
@@ -9,7 +9,7 @@ const {VueLoaderPlugin}=require('vue-loader/dist/index')
 //webpack配置文件
 module.exports = {
   //设置模式 development 开发阶段设置，production 准备打包上线的时候设置
-  //mode:"development",
+  mode:"development",
   //设置source-map 建立js映射文件，方便调试与发现错误
   //devtool:"source-map",
   //入口
@@ -18,6 +18,48 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./build"),
     filename: "js/bundle.js"
+  },
+  //文件打包过大设置
+  performance:{
+    hints:"warning",
+    maxAssetSize:30000000, //生成文件的最大体积 整数类型（以字节为单位）
+    maxEntrypointSize:50000000,//入口文件的最大体积
+    //只给出js文件或css文件的性能提示
+    assetFilter:function(assetFilename){
+
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js')
+    }
+  },
+  devServer:{
+    //如果一个资源没有从webpack打包的文件中得到支持，则会从contentBase中的地址获得支持（主要是一些不被打包的静态资源）
+    //在开发阶段，在使用devServer时没有copy-webpack来进行静态资源的复制，有可能会找不到，在这时候使用contentBase
+    contentBase:"./public",
+
+    //模块热更新，无需刷新页面，只变换改变模块，可保留某些状态
+    hot:true,
+
+    //host 地址
+
+    //port 端口号
+
+    //是否自动打开浏览器
+    open:true,
+
+    //compress  是否队静态资源进行gzip压缩
+
+    //跨域代理
+    // proxy:{
+    //   "/api":{
+    //      taget:'代理的地址',
+    //      pathRewrite:{
+    //          "^/api":''   //重写地址为空
+    //        },
+
+    //      secure:false,    //是否接受没有证书的https请求 false为允许
+
+    //      changeOrigin:true  //请求时是否换成代理的源地址 
+    //   }
+    // }
   },
   //loader配置
   module:{
@@ -90,15 +132,19 @@ module.exports = {
 
     
     //webpack自带插件 可定义一些打包时的变量
-    // new DefinPlugin({
-    //   BASE_URL:''
-    // })
+    new DefinePlugin({
+      BASE_URL:"'./'",
+      //vue是否向下兼容vue2
+      __VUE_OPTIONS_API__:true,
+      //在生产阶段是否需要使用vue的调试工具
+      __VUE_PROD_DEVTOOLS__:false
+    }),
 
     //webpack打包时拷贝文件到打包位置（静态资源等）
     // new CopyWebpackPlugin({
     //   patterns:[
     //     {
-    //       from:'需要拷贝的文件地址',
+    //       from:'需要拷贝的文件地址（一般情况为pubilc中的静态资源）',
     //       to:'到哪里 到打包文件最外层时可以忽略',
     //       
     //       配置忽略文件  
